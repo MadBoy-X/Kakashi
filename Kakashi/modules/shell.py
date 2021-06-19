f93e8eb5681548b673917020cec54ee79ae21bc7
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-19 17:32:06 +0530

    Update shell.py

--- Kakashi/modules/shell.py
+++ Kakashi/modules/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from YoneRobot import LOGGER, dispatcher
-from YoneRobot.modules.helper_funcs.chat_status import dev_plus
+from Kakashi import LOGGER, dispatcher
+from Kakashi.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
