7f892c7aca6cf57b632a76742ccc03a01cd753b9
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-19 16:42:45 +0530

    Update __weather.py

--- Kakashi/modules/__weather.py
+++ Kakashi/modules/__weather.py
@@ -1,4 +1,4 @@
-from YoneRobot import telethn as tbot
+from Kakashi import telethn as tbot
 import io
 import os
 import time
@@ -9,9 +9,9 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from YoneRobot import *
+from Kakashi import *
 
-from YoneRobot.events import register
+from Kakashi.events import register
 
 
 async def is_register_admin(chat, user):
