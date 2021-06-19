52679d236de826f7bad594c40df22a653efbf5a5
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-19 15:20:03 +0530

    Update pluginshelper.py

--- Kakashi/pyrogramee/pluginshelper.py
+++ Kakashi/pyrogramee/pluginshelper.py
@@ -12,8 +12,8 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from YoneRobot  import OWNER_ID, SUPPORT_CHAT
-from YoneRobot import pbot
+from Kakashi  import OWNER_ID, SUPPORT_CHAT
+from Kakashi import pbot
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
