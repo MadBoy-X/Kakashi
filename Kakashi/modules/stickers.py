ee75fae00ab8a536ab9f9290dd7481fd4b3b73e2
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-19 17:33:35 +0530

    Update stickers.py

--- Kakashi/modules/stickers.py
+++ Kakashi/modules/stickers.py
@@ -12,8 +12,8 @@ from telegram import TelegramError, Update
 from telegram.ext import run_async, CallbackContext
 from telegram.utils.helpers import mention_html
 
-from YoneRobot import dispatcher
-from YoneRobot.modules.disable import DisableAbleCommandHandler
+from Kakashi import dispatcher
+from Kakashi.modules.disable import DisableAbleCommandHandler
 
 combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
