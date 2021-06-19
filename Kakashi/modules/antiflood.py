e3d2a7b04f5851e2e0adb25098a77ce4d2d3fdd8
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-19 16:50:58 +0530

    Update antiflood.py

--- Kakashi/modules/antiflood.py
+++ Kakashi/modules/antiflood.py
@@ -4,15 +4,15 @@ import re
 
 from telegram import Message, Chat, Update, User, ChatPermissions
 
-from YoneRobot import TIGERS, WOLVES, dispatcher
-from YoneRobot.modules.helper_funcs.chat_status import (
+from Kakashi import TIGERS, WOLVES, dispatcher
+from Kakashi.modules.helper_funcs.chat_status import (
     bot_admin,
     is_user_admin,
     user_admin,
     user_admin_no_reply,
 )
-from YoneRobot.modules.log_channel import loggable
-from YoneRobot.modules.sql import antiflood_sql as sql
+from Kakashi.modules.log_channel import loggable
+from Kakashi.modules.sql import antiflood_sql as sql
 from telegram.error import BadRequest
 from telegram.ext import (
     CallbackContext,
@@ -23,10 +23,10 @@ from telegram.ext import (
     run_async,
 )
 from telegram.utils.helpers import mention_html, escape_markdown
-from YoneRobot.modules.helper_funcs.string_handling import extract_time
-from YoneRobot.modules.connection import connected
-from YoneRobot.modules.helper_funcs.alternate import send_message
-from YoneRobot.modules.sql.approve_sql import is_approved
+from Kakashi.modules.helper_funcs.string_handling import extract_time
+from Kakashi.modules.connection import connected
+from Kakashi.modules.helper_funcs.alternate import send_message
+from Kakashi.modules.sql.approve_sql import is_approved
 
 FLOOD_GROUP = 3
 
