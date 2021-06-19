33d0e4236408509f7292f44be3047fe8543b00cf
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-19 15:15:22 +0530

    Update feds_sql.py

--- Kakashi/modules/sql/feds_sql.py
+++ Kakashi/modules/sql/feds_sql.py
@@ -1,7 +1,7 @@
 import threading
 
-from YoneRobot import dispatcher
-from YoneRobot.modules.sql import BASE, SESSION
+from Kakashi import dispatcher
+from Kakashi.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 from telegram.error import BadRequest, Unauthorized
 
