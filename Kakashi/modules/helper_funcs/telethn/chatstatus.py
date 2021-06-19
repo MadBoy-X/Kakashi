ebcb03eb808adef3383344b971c2d42acfc7ab84
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-19 14:57:20 +0530

    Update chatstatus.py

--- Kakashi/modules/helper_funcs/telethn/chatstatus.py
+++ Kakashi/modules/helper_funcs/telethn/chatstatus.py
@@ -1,5 +1,5 @@
-from YoneRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from YoneRobot import DRAGONS
+from Kakashi.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from Kakashi import DRAGONS
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
