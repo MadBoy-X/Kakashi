cfe7c169665f615887f5c3d1854d5cd7ba942269
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-19 16:17:27 +0530

    Update __country.py

--- Kakashi/modules/__country.py
+++ Kakashi/modules/__country.py
@@ -5,8 +5,8 @@ from telethon.utils import get_input_location
 import flag
 import html, os
 from countryinfo import CountryInfo
-from YoneRobot import telethn as borg
-from YoneRobot.events import register
+from Kakashi import telethn as borg
+from Kakashi.events import register
 
 
 @register(pattern="^/country (.*)")
@@ -101,7 +101,7 @@ Time Zones:- {tom}
 Top Level Domain:- {lanester}
 wikipedia:- {wiki}</b>
 
-Gathered By Yone.</b>
+Gathered By Kakashi Hatake.</b>
 """
     
     
