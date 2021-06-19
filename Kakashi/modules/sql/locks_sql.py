ec24a6da6f95684b3cfd54f12e04360992f6aa0f
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-19 15:16:05 +0530

    Update locks_sql.py

--- Kakashi/modules/sql/locks_sql.py
+++ Kakashi/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from YoneRobot.modules.sql import SESSION, BASE
+from Kakashi.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
