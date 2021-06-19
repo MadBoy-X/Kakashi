8b2089f68e2cc241f2bb2dc53c46b5e4ba9ab71e
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-19 17:09:17 +0530

    Update debug.py

--- Kakashi/modules/debug.py
+++ Kakashi/modules/debug.py
@@ -5,8 +5,8 @@ from telethon import events
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
-from YoneRobot import telethn, dispatcher
-from YoneRobot.modules.helper_funcs.chat_status import dev_plus
+from Kakashi import telethn, dispatcher
+from Kakashi.modules.helper_funcs.chat_status import dev_plus
 
 DEBUG_MODE = False
 
