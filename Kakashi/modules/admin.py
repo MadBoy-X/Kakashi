7be9f86edd7ffd62e1566da768ec01459229b4cb
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-19 16:48:44 +0530

    Update admin.py

--- Kakashi/modules/admin.py
+++ Kakashi/modules/admin.py
@@ -5,9 +5,9 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from YoneRobot import DRAGONS, dispatcher
-from YoneRobot.modules.disable import DisableAbleCommandHandler
-from YoneRobot.modules.helper_funcs.chat_status import (
+from Kakashi import DRAGONS, dispatcher
+from Kakashi.modules.disable import DisableAbleCommandHandler
+from Kakashi.modules.helper_funcs.chat_status import (
     bot_admin,
     can_pin,
     can_promote,
@@ -15,19 +15,19 @@ from YoneRobot.modules.helper_funcs.chat_status import (
     user_admin,
     ADMIN_CACHE,
 )
-from YoneRobot.helper_extra.admin_rights import (
+from Kakashi.helper_extra.admin_rights import (
     user_can_pin,
     user_can_promote,
     user_can_changeinfo,
 )
 
-from YoneRobot.modules.helper_funcs.extraction import (
+from Kakashi.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from YoneRobot.modules.log_channel import loggable
-from YoneRobot.modules.helper_funcs.alternate import send_message
-from YoneRobot.modules.helper_funcs.alternate import typing_action
+from Kakashi.modules.log_channel import loggable
+from Kakashi.modules.helper_funcs.alternate import send_message
+from Kakashi.modules.helper_funcs.alternate import typing_action
 
 
 @run_async
