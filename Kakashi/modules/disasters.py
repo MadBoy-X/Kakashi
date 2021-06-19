73645e1a0c4ee387f8aea73e74e729fa98b61ae6
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-19 17:12:46 +0530

    Update disasters.py

--- Kakashi/modules/disasters.py
+++ Kakashi/modules/disasters.py
@@ -3,7 +3,7 @@ import json
 import os
 from typing import Optional
 
-from YoneRobot import (
+from Kakashi import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -13,18 +13,18 @@ from YoneRobot import (
     WOLVES,
     dispatcher,
 )
-from YoneRobot.modules.helper_funcs.chat_status import (
+from Kakashi.modules.helper_funcs.chat_status import (
     dev_plus,
     sudo_plus,
     whitelist_plus,
 )
-from YoneRobot.modules.helper_funcs.extraction import extract_user
-from YoneRobot.modules.log_channel import gloggable
+from Kakashi.modules.helper_funcs.extraction import extract_user
+from Kakashi.modules.log_channel import gloggable
 from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
 
-ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "YoneRobot/elevated_users.json")
+ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "Kakashi/elevated_users.json")
 
 
 def check_user_id(user_id: int, context: CallbackContext) -> Optional[str]:
