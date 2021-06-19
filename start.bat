6878fd1dcf3293bdeeb028d6dc6d6adc9cb1ccc2
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-19 19:42:24 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Yone Robot
+TITLE Kakashi Hatake
 :: Enables virtual env mode and then starts Rika
-env\scripts\activate.bat && py -m YoneRobot
+env\scripts\activate.bat && py -m Kakashi
