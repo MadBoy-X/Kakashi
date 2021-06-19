0a5a420798657febf352ed45ddedf6190f6588d6
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-19 15:51:19 +0530

    Update sample_config.py

--- Kakashi/sample_config.py
+++ Kakashi/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/YoneRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/Kakashi/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -15,11 +15,11 @@ class Config(object):
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
     API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
+    API_HASH = "awoooo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Kittu"
-    SUPPORT_CHAT = "Kora_Support"  # Your own group for support, do not add the @
+    OWNER_ID = 1883752632 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Its_JassManak"
+    SUPPORT_CHAT = "Kakashi_Support"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001253661229
     )  # Prints any new group the bot is added to, prints just the name and ID.
