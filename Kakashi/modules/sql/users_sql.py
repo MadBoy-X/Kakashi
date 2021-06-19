04692f67780dfb305040e86daa0a3feeb63b7afd
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-06-19 15:17:51 +0530

    Update users_sql.py

--- Kakashi/modules/sql/users_sql.py
+++ Kakashi/modules/sql/users_sql.py
@@ -1,7 +1,7 @@
 import threading
 
-from YoneRobot import dispatcher
-from YoneRobot.modules.sql import BASE, SESSION
+from Kakashi import dispatcher
+from Kakashi.modules.sql import BASE, SESSION
 from sqlalchemy import (
     Column,
     ForeignKey,
